ded6b28af4dd5d10a7783816337af00c7f011a42
Keila Vargas <keila.vargas0365@gmail.com>
2023-07-18 16:32:58 -0500

    before breaking anything

--- js/weather_map.js
+++ js/weather_map.js
@@ -3,47 +3,14 @@
 const OPEN_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/forecast';
 let searchBox = document.querySelector('#user-input');
 let searchBtn = document.querySelector('#search');
-function initializedMap() {
-    mapboxgl.accessToken = MAPBOX_TOKEN;
-    const mapOptions = {
-        container: "map",
-        // accepts the id, but you don't need a hashtag because this only accepts the id
-        style: "mapbox://styles/mapbox/outdoors-v12",
-        zoom:8,
-        center: [-95.712891, 37.09024]
-    }
-    return new mapboxgl.Map(mapOptions);
-}
-// const latLng = [-95.712891, 37.09024];
 const map = initializedMap();
-function createMarker(map){
-    map.center
-    const marker = new mapboxgl.Marker()
-        .setLngLat(latLng)
-        .addTo(map);
-    // createPopup(marker)
-}
+const marker = createMarker(map);
 
-// Simple way to create URL for request based on coordinates
+// functions
+///////////////////////////////////////////////////////////////////////////////////
 function getWeatherURL(coordinates) {
     return `${OPEN_WEATHER_URL}?lat=${coordinates[1]}&lon=${coordinates[0]}&units=imperial&appid=${OPEN_WEATHER_APPID}`;
 }
-
-// geocode(userInput),MAPBOX_TOKEN.then(function(results){
-
-// });
-function getWeatherLatLng (map) {
-    const coords = [map.getCenter().lng, map.getCenter().lat];
-    return coords;
-}
-
-// const ALAMO_COORDINATES = ['29.4260', '-98.4861'];
-// const GET_COORDINATES = [];
-
-// TODO: log URL
-
-// console.log(getWeatherURL(ALAMO_COORDINATES[0], ALAMO_COORDINATES[1]));
-
 function displayData(data) {
     const resultElement = document.querySelector("#result");
     resultElement.innerHTML = "";
@@ -57,21 +24,29 @@ function displayData(data) {
 
     filteredData.forEach(item => {
         const emptyDiv = document.createElement("div");
-        emptyDiv.textContent = `
-            ${item.dt_txt.split(" ")[0]},
-            ${item.main.temp_min}°F / ${item.main.temp_max}°F 
-            Description ${item.weather[0].description},
-            Humidity: ${item.main.humidity},
-             Wind: ${item.wind.speed},
-             Feels like: ${item.main.feels_like}°F,
-            Pressure: ${item.main.pressure}
-             `;
+        emptyDiv.classList.add("card", "weather", "d-flex", "flex-column", "text-center", "flex-grow-1", );
         resultElement.appendChild(emptyDiv);
+        console.log(data.city.name);
+        emptyDiv.innerHTML = `
+<p class="city">${data.city.name},</p>
+               <p class="date"> ${item.dt_txt.split(" ")[0]},</p>
+               <p class="temp"> ${item.main.temp_min}°F / ${item.main.temp_max}°F </p>
+               <p class="description"> Description ${item.weather[0].description}, </p>
+               <p class="icon"> <img src="http://openweathermap.org/img/w/${item.weather[0].icon}.png" alt=""></p>
+               <p class="humidity"> Humidity: ${item.main.humidity},</p>
+                <p class="wind"> Wind: ${item.wind.speed},</p
+                <p class="feels-like-temp"> Feels like: ${item.main.feels_like}°F,</p>
+                <p class="pressure">Pressure: ${item.main.pressure}</p>
+             `;
     });
 }
-
-function getData(map) {
-    const URL = getWeatherURL(getWeatherLatLng(map));
+// function changeBackgrounds(){
+//     if ([item.list.weather.icon ===  01d]){
+//     return
+//     }
+// }
+function getDataWithCoords(lng,lat){
+    const URL = getWeatherURL([lng,lat]);
     $.ajax(URL)
         .done(data => {
             console.log(data);
@@ -79,105 +54,65 @@ function getData(map) {
         })
         .fail(console.error);
 }
+function initializedMap() {
+    mapboxgl.accessToken = MAPBOX_TOKEN;
+    const mapOptions = {
+        container: "map",
+        // accepts the id, but you don't need a hashtag because this only accepts the id
+        style: "mapbox://styles/mapbox/outdoors-v12",
+        zoom: 8,
+        center: [-95.712891, 37.09024]
+    }
+    getDataWithCoords(-95.712891,37.09024);
+    return new mapboxgl.Map(mapOptions);
+}
 
-getData(map);
+// const latLng = [-95.712891, 37.09024];
+function createMarker(map){
+    return new mapboxgl.Marker({
+        draggable: true,
+    })
+        .setLngLat(map.getCenter())
+        .addTo(map);
+}
 
 
 
-// createMarker(map);
 
-//    Events
+function searchInput(map, searchBox) {
+    geocode(searchBox.value, MAPBOX_TOKEN).then((data)=>{
+        console.log(data);
+        getDataWithCoords(data[0], data[1]);
+
+        marker.setLngLat(data)
+            // .addTo(map)
+        console.log(marker.getLngLat());
+
+        map.flyTo({
+            center:data,
+            zoom: 10
+        })
+    })
+}
+///////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////
+// Events
+
 
 searchBox.addEventListener('input', (e)=>{
     console.log(e.target.value);
 });
-searchBtn.addEventListener('click', () => {})
-map.on('mouseup', ()=>{
-    getData(map);
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(searchBox.value);
+    searchInput(map, searchBox);
+})
+map.on('mouseup', (e)=>{
+    console.log(e.lngLat);
+    getDataWithCoords(e.lngLat.lng, e.lngLat.lat);
+    marker.setLngLat(e.lngLat);
 });
-map.on('click mouseup', () => {
-    getData(map);
-});
+///////////////////////////////////////////////////////////////////////////////////
 
-
-
-
-
-
-
-// const dataArray = getArrayFromAPI();
-//
-// for (let i = 0; i < dataArray.length; i++) {
-//     const item = dataArray[i];
-//     const humidity = item.main.humidity;
-//     const pressure = item.main.pressure;
-//
-//     console.log(humidity);
-//     console.log(pressure);
-// }
-
-
-
-
-
-
-
-// TODO: log various parts of the API
-
-// $.ajax(URL).done(data => {
-//     console.log(data);
-//     // TODO: log the city name
-//     // TODO: log the first three-hour forecast block
-//     // TODO: log the humidity for the first three-hour block
-// }).fail(console.error);
-
-
-// TODO: log the humidity for all days
-
-// $.ajax(getWeatherURL(...ALAMO_COORDINATES))
-//     .done((data) => {
-//
-//         data.list.forEach((day, index) => {
-//             if (index % 8 === 0) {
-//                 console.log(day.main.humidity);
-//             }
-//         });
-//
-//         // OR
-//
-//         for (let i = 0; i < data.list.length; i += 8) {
-//             console.log(data.list[i].main.humidity);
-//         }
-//
-//     })
-//     .fail(console.error);
-
-
-// TODO: log the min and max temp for each day
-
-// $.ajax(getWeatherURL(...ALAMO_COORDINATES))
-//     .done(data => {
-//         console.log(data);
-//         const minMaxTemps = returnMinMaxTemps(data);
-//         minMaxTemps.forEach(minMaxTemp => {
-//             console.log(minMaxTemp);
-//         });
-//     })
-//     .fail(console.error);
-
-
-
-// $.get("http://api.openweathermap.org/data/2.5/weather", {
-//     APPID: OPEN_WEATHER_APPID,
-//     lat:    29.423017,
-//     lon:   -98.48527,
-//     units: "imperial"
-// }).done(function(data) {
-//     console.log(data);
-// });
-// function test(datasets) {
-//     console.log("Testing weather" + JSON.stringify(datasets));
-//     const test = JSON.parse(datasets);
-// console.log("Testing2: " + test.coord);
-//     }
 
